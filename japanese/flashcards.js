83029c9611260d840b0e2ff769955d929236e5f6
John Boctor <jboctor@outlook.com>
2018-02-16 15:57:45 -0600

    First word displaying and prev/next buttons also displayed

--- japanese/flashcards.js
+++ japanese/flashcards.js
@@ -1,2 +1,40 @@
-var words = JSON.parse(wordbank);
-console.log(words);
+$(document).ready(function () {
+    var currentWord;
+    var wordBank       = [];
+    var completedWords = [];
+
+    $("#translation").hide();
+    $.ajax({
+        url: "wordbank.json"
+    }).done(function (data) {
+        setUpFlashCards(JSON.parse(data));
+        getNextWord();
+    });
+
+    var setUpFlashCards = function (words) {
+        categories = words.categories;
+        for (category in categories) {
+            for (word in categories[category]) {
+                wordBank.unshift(categories[category][word]);
+            }
+        }
+        $("#flashcard").click(function () {
+            flipped = $("#translation").is(":visible");
+            if (flipped) {
+                $("#translation").hide();
+                $("#word").show();
+            } else {
+                $("#translation").show();
+                $("#word").hide();
+            }
+        })
+    };
+
+    var getNextWord = function () {
+        currentWord = wordBank.pop();
+        $("#word").html(
+            "<ruby>" + currentWord["word"] + "<rt>" + currentWord["furigana"] + "</rt></ruby>"
+        )
+        $("#translation").html(currentWord["translation"]);
+    }
+});
