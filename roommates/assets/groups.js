9e14096153d9f4b9543e3a23819f9798114caf44
John Boctor <jboctor@outlook.com>
2018-10-30 15:16:13 -0500

    Cleaned things up

--- roommates/assets/groups.js
+++ roommates/assets/groups.js
@@ -19,34 +19,34 @@ var groups = {
     handleCsv: function (contents) {
         parsedContents     = Papa.parse(contents);
         parsedContentsData = parsedContents.data;
-        groupedPeople      = [[], [], [], [], [], [], [], [], [], []];
+        numberOfGroups     = 10;
+        groupedPeople      = new Array(numberOfGroups);
         for(var i = parsedContentsData.length - 1; i >= 0; i--) {
-            row  = parsedContentsData.splice(Math.floor(Math.random() * parsedContentsData.length), 1)[0];
-            name = row[0].toLowerCase() + ' ' + row[1].toLowerCase();
-            groupedPeople[i % 10].push(name);
+            row   = parsedContentsData.splice(Math.floor(Math.random() * parsedContentsData.length), 1)[0];
+            name  = row[0].toLowerCase() + ' ' + row[1].toLowerCase();
+            index = i % numberOfGroups;
+
+            groupedPeople[index] = groupedPeople[index] || [];
+            groupedPeople[index].push(name);
         }
-        groups.printgroups(groupedPeople[0], $('#group1'));
-        groups.printgroups(groupedPeople[1], $('#group2'));
-        groups.printgroups(groupedPeople[2], $('#group3'));
-        groups.printgroups(groupedPeople[3], $('#group4'));
-        groups.printgroups(groupedPeople[4], $('#group5'));
-        groups.printgroups(groupedPeople[5], $('#group6'));
-        groups.printgroups(groupedPeople[6], $('#group7'));
-        groups.printgroups(groupedPeople[7], $('#group8'));
-        groups.printgroups(groupedPeople[8], $('#group9'));
-        groups.printgroups(groupedPeople[9], $('#group10'));
+        groups.printgroups(groupedPeople, $('#groups'));
     },
 
-    printgroups: function (group, element) {
-        var table = $('<table class="table"></table>')
-        for (var i = 0; i < group.length; i++) {
-            if (i % 4 == 0) {
-                var row = $('<tr></tr>');
-                table.append(row);
+    printgroups: function (groups, element) {
+        element.html('');
+        for (var i = 0; i < groups.length; i++) {
+            var group = groups[i];
+            var table = $('<table class="table"></table>')
+            for (var j = 0; j < group.length; j++) {
+                if (j % 4 == 0) {
+                    var row = $('<tr></tr>');
+                    table.append(row);
+                }
+                row.append('<td>' + group[j] + '</td>');
             }
-            row.append('<td>' + group[i] + '</td>');
-        };
-        element.html(table);
+            element.append('<div class="row"><div class="col"><h2>group ' + (i + 1) + '</h2></div></div>');
+            element.append('<div class="row"><div class="col"><div class="container-fluid" id="group1">' + $('<div>').append(table.clone()).html() + '</div></div></div>');
+        }
     }
 }
 
