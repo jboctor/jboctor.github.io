1e8d0ec30c4fa935e5ae10b5f2d45a94063db559
John Boctor <jboctor@outlook.com>
2019-02-27 10:20:44 -0600

    Weighted chars and bug fix

--- wordsearch/search.js
+++ wordsearch/search.js
@@ -1,22 +1,63 @@
 var wordSearch = {
-    possibleChars : 'abcdefghijklmnopqrstuvwxyz',
+    possibleChars : [
+        ['a', 8.12],
+        ['b', 1.49],
+        ['c', 2.71],
+        ['d', 4.32],
+        ['e', 12.02],
+        ['f', 2.30],
+        ['g', 2.03],
+        ['h', 5.92],
+        ['i', 7.31],
+        ['j', 0.10],
+        ['k', 0.69],
+        ['l', 3.98],
+        ['m', 2.61],
+        ['n', 6.95],
+        ['o', 7.68],
+        ['p', 1.82],
+        ['q', 0.11],
+        ['r', 6.02],
+        ['s', 6.28],
+        ['t', 9.10],
+        ['u', 2.88],
+        ['v', 1.11],
+        ['w', 2.09],
+        ['x', 0.17],
+        ['y', 2.11],
+        ['z', 0.07]
+    ],
     gridSize : 0,
     grid : null,
     track: [],
     createGrid : function (size)
     {
+        var chars = this.createWeightedChars();
         this.gridSize = size;
         this.grid = {};
         for (i = 0; i < this.gridSize; i++) {
             this.grid[i] = {};
             for (j = 0; j < this.gridSize; j++) {
-                this.grid[i][j] = this.possibleChars.charAt(
+                this.grid[i][j] = chars[
                     Math.floor(
-                        Math.random() * this.possibleChars.length
+                        Math.random() * chars.length
                         )
-                    );
+                ];
             }
         }
+    },
+
+    createWeightedChars : function ()
+    {
+        var weightedChars = [];
+        for (i = 0; i < this.possibleChars.length; i++) {
+            var letter    = this.possibleChars[i][0];
+            var multiples = this.possibleChars[i][1] * 100;
+            for (j = 0; j < multiples; j++) {
+                weightedChars.push(letter);
+            }
+        }
+        return weightedChars;
     },
 
     printGrid : function()
@@ -61,8 +102,7 @@ var wordSearch = {
                 newGrid[i] = {};
                 for (j = y - 1; j <= y + 1; j++) {
                     if (i > -1 && i < this.gridSize &&
-                        j > -1 && j < this.gridSize &&
-                        i != x && j != y)
+                        j > -1 && j < this.gridSize)
                     {
                         newGrid[i][j] = this.grid[i][j];
                     } else {
